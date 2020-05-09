271d95d9b5f8131a35409863bb371d38bf4c258f
Adithya Bhat <adithyahayagreevb@emiratesnbd.com>
2020-05-09 11:38:02 +0400

    Initial implementation

---
+++ VIPER Demo/Modules/Presenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER Demo
+//
+//  Created by Adithya hayagreeva on 5/8/20.
+//  Copyright © 2020 Emirates NBD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
